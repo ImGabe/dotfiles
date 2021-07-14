d477320d60e0c5c6f23889467111001acbd8a065
ImGabe <gabrielpmonte@hotmail.com>
2021-07-14 09:08:42 -0300

    remove spaces and change description

--- flake.nix
+++ flake.nix
@@ -1,31 +1,23 @@
 {
-  description = "Dotfiles flake";
+  description = "My NixOS configuration";
 
   inputs = {
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     nur.url = "github:nix-community/NUR";
-
   };
 
   outputs = { self, nixpkgs, home-manager, nur, ... }@inputs: {
-
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
-
       modules = [
         { nixpkgs.overlays = [ nur.overlay ]; }
-
         ./configuration.nix
         ./home.nix
-
         home-manager.nixosModules.home-manager
       ];
-
       specialArgs = { inherit inputs system; };
     };
-
   };
 }
