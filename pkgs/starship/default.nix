bebba2c6c1e0885bb5a777c1f8fcfe7950d6cc57
ImGabe <gabrielpmonte@hotmail.com>
2021-10-31 12:49:45 -0300

    remove zsh integration

--- pkgs/starship/default.nix
+++ pkgs/starship/default.nix
@@ -1,7 +1,7 @@
 {
   programs.starship = {
     enable = true;
-    enableZshIntegration = true;
+
     # Configuration written to ~/.config/starship.toml
     settings = {
       add_newline = false;
